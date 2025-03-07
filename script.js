0d54d73d21ad96d0cb51dacdcdff756859fccae5
refatkhan <refatkhan897@gmail.com>
2025-03-08 01:39:09 +0600

    blogs

---
+++ script.js
@@ -0,0 +1,12 @@
+// change background color with math function 
+function changeBackgroundColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+}
+// Date in nav 
+const dateContainer = document.getElementById("date");
+const p = document.createElement("p");
+let dateString = new Date().toDateString();
+let formattedDate = dateString.replace(" ", "<br>");
+p.innerHTML = formattedDate; 
+dateContainer.appendChild(p);

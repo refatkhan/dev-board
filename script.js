8f90732dc07a22013f7172c677ba5382df637029
refatkhan <refatkhan897@gmail.com>
2025-03-09 02:44:56 +0600

    task assigned decreased with buton click

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-// change background color with math function 
+// change background color with math function
 function changeBackgroundColor() {
-    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-    document.body.style.backgroundColor = randomColor;
+  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  document.body.style.backgroundColor = randomColor;
 }
-// Date in nav 
+// Date in nav
 const dateContainer = document.getElementById("date");
 const p = document.createElement("p");
 let dateString = new Date().toDateString();
 let formattedDate = dateString.replace(" ", "<br>");
-p.innerHTML = formattedDate; 
+p.innerHTML = formattedDate;
 dateContainer.appendChild(p);
 
-// redirect 
-document.getElementById('blog').addEventListener('click',function(){
-    window.location.href='blog.html'
-})
+// redirect
+document.getElementById("blog").addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
 
-// button alert 
+// button related DOM
+
+const taskCount = document.getElementById("task-assigned");
+let totalCount = parseInt(taskCount.innerText);
 let disabledButton = 0;
-const buttons = document.querySelectorAll('.complete-btn');
-for(button of buttons){
-    button.addEventListener('click',function(event){
-        alert('Board Updated');;
-        event.target.setAttribute('disabled',true);
-        disabledButton++;
-        if(disabledButton === buttons.length){
-            alert('all done');
-        }
-    })
+const buttons = document.querySelectorAll(".complete-btn");
+for (button of buttons) {
+  button.addEventListener("click", function (event) {
+    alert("Board Updated");
+    event.target.setAttribute("disabled", true);
+    disabledButton++;
+    totalCount--;
+    taskCount.innerText = totalCount;
+
+    if (disabledButton === buttons.length) {
+      alert("all done");
+    }
+  });
 }

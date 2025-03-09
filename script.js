351e5facf0cf0b325acadaee3f026575e0ade2c0
refatkhan <refatkhan897@gmail.com>
2025-03-09 16:05:27 +0600

    fix javascript file location

--- script.js
+++ script.js
@@ -9,11 +9,12 @@ const p = document.createElement("p");
 let dateString = new Date().toDateString();
 let formattedDate = dateString.replace(" ", "<br>");
 p.innerHTML = formattedDate;
+p.style.padding = '20px'
 dateContainer.appendChild(p);
 
 // redirect
 document.getElementById("blog").addEventListener("click", function () {
-  window.location.href = "blog.html";
+  window.location.href = "./blog.html";
 });
 
 // button related DOM
@@ -25,17 +26,30 @@ let disabledButton = 0;
 const buttons = document.querySelectorAll(".complete-btn");
 for (button of buttons) {
   button.addEventListener("click", function (event) {
-    alert("Board Updated");
+    alert("Board updated successfully");
     event.target.setAttribute("disabled", true);
     disabledButton++;
     totalCount--;
     totalNavCount++;
-    
     taskCount.innerText = totalCount;
     navCount.innerText = totalNavCount;
-
     if (disabledButton === buttons.length) {
-      alert("all done");
+      alert("Congrates!!! You have completed all the current task");
     }
   });
 }
+
+// activity history 
+const clearHisotry = document.getElementById('historyClear');
+const eventDetails = document.getElementById('eventHistory');
+ for(button of buttons){
+    button.addEventListener('click',function(){
+        const taskTitle = document.querySelectorAll('.task-title').innerText;
+        const time = new Date().toLocaleTimeString();
+        const logMessage = 'You have completed the task ' + taskTitle + ' at ' + time;
+        eventDetails.innerHTML += '<div class="bg-[#F4F7FF] rounded-lg p-5">' + logMessage + '</div>';
+    })
+ }
+ clearHisotry.addEventListener('click', function() {
+    eventDetails.innerHTML = ''; 
+  });
